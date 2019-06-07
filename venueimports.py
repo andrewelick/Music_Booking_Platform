8a29ded2b89d9e6aaacb8fcd70d0bdc2c6d927bc
Andrew Elick <andy.elick@gmail.com>
2019-06-06 23:28:59 -0500

    Inital push of Bluff Tour app to Heroku

--- venueimports.py
+++ venueimports.py
@@ -7,6 +7,7 @@ from string import Template
 from bs4 import BeautifulSoup
 from requests import get
 import urllib.request
+import urllib.parse
 import pymysql
 import hashlib #Used for password hashing
 import datetime
@@ -15,3 +16,11 @@ import requests
 from email.mime.multipart import MIMEMultipart #Used for sending emails
 from email.mime.text import MIMEText #Used for sending emails
 import stripe #Used for payment processing
+import shutil
+import json
+import base64
+import re
+import bcrypt
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
+from requests.auth import HTTPBasicAuth
