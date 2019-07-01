94ecaa8a5936f8020f71f6347141f0b8db835af4
Andrew Elick <andy.elick@gmail.com>
2019-06-30 23:36:57 -0500

    Working on iframe saving

--- venuehandler.py
+++ venuehandler.py
@@ -606,8 +606,6 @@ def get_artist_media(uid):
             c.execute("""SELECT media_id, src FROM artist_media WHERE uid = %s""", (uid,))
             all_media = c.fetchall()
 
-            print (all_media)
-
             return json.dumps({'success': all_media})
         except Exception as e:
             print (e)
